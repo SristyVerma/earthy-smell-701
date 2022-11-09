28b7c6b8606d3036a439dc5fafa60ad728cc6c98
SristyVerma <sristyel2016@gmail.com>
2022-11-09 23:11:54 +0530

    navbar and footer done

--- express_food/src/App.js
+++ express_food/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import "./Styles/Navbar.css";
+import "./Styles/Footer.css";
 
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Articles from "./Components/Articles";
+import Directory from "./Components/Directory";
+import HotPicks from "./Components/HotPicks";
+import Home from "./Components/Home";
+import Help from "./Components/Help";
+import MealDeals from "./Components/MealDeals";
+
+import KindMoment from "./Components/KindMoment";
+import SignUp from "./Components/SignUp";
+import Contacts from "./Components/Contacts";
+import AboutUs from "./Components/AboutUs";
+import Footer from "./Components/Footer";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Navbar />
 
-export default App;
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/articles" element={<Articles />}></Route>
+        <Route exact path="/directory" element={<Directory />}></Route>
+        <Route exact path="/help" element={<Help />}></Route>
+        <Route exact path="/hotpicks" element={<HotPicks />}></Route>
+        <Route exact path="/kindmoments" element={<KindMoment />}></Route>
+        <Route exact path="/mealdeals" element={<MealDeals />}></Route>
+        <Route exact path="/signup" element={<SignUp />}></Route>
+        <Route exact path="/contacts" element={<Contacts />}></Route>
+        <Route exact path="/about" element={<AboutUs />}></Route>
+      </Routes>
+      <Footer />
+    </>
+  );
+};
+
+export { App };
--- express_food/src/index.js
+++ express_food/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-boe6etc7ie80l2yk.us.auth0.com"
+    clientId="tBJsvWOrPCRQULGj7AV4GXxfDTkmMpDD"
+    redirectUri={window.location.origin}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
